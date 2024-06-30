165aaacaad9a926777e08edb2be790235076b925
John Heusinger <john.heusinger@gmail.com>
2024-06-30 18:18:37 +0200

    Fix formatting

--- src/random_agent.py
+++ src/random_agent.py
@@ -11,24 +11,23 @@ y = list()
 number_of_episodes = 1000
 
 for i in range(number_of_episodes):
-
     done = False  # Reset game state
     state = env.reset()[0]  # For a new run, reset environment
 
     while not done:
         random_action = env.action_space.sample()  # Choose a random action
-        state_p, reward, terminated, truncated, _ = env.step(random_action)  # take a step and get outcome
+        state_p, reward, terminated, truncated, _ = env.step(
+            random_action
+        )  # take a step and get outcome
         X.append([*state.tolist(), int(random_action)])
         y.append(state_p.tolist())
         state = state_p  # s is now s'
         done = terminated or truncated  # check if game over
 
-#print(f"X: {X[:5]}, \n y: {y[:5]}")
+# print(f"X: {X[:5]}, \n y: {y[:5]}")
 X = np.array(X)
 y = np.array(y)
-data = [X,y]
+data = [X, y]
 
 with open("data/random_agent_data_np.pkl", "wb") as file:
     file.write(pickle.dumps(data))
-    
-
